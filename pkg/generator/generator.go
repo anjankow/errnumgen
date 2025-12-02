03e1f96ac202d5d131fc0fd5b43fd0651865bb4e
anjankow <anjankow@protonmail.com>
2025-12-02 13:24:51 +0100

    find results in the filtered functions

--- pkg/generator/generator.go
+++ pkg/generator/generator.go
@@ -56,68 +56,115 @@ func New(dir string) (Generator, error) {
 
 func (g *Generator) ParseErrs() error {
 	for _, pkg := range g.pkgs {
-		debugPrintln("PACKAGE NAME: ", pkg.Name)
 
+		_, debugPrintln := debugPrint(pkg.PkgPath)
+
+		g.filterPackageDecls(pkg)
+		debugPrintln("filtered package decls")
+
+		// The remaining declarations are now only function declarations that return an error
 		for _, stxFile := range pkg.Syntax {
-			tokenFile := pkg.Fset.File(stxFile.FileStart)
-			filename := tokenFile.Name()
+			filename := getFilename(pkg, stxFile)
+			debugPrintf, _ := debugPrint(filename)
 
-			// Filter functions that return an error
-			j := 0
-			for _, decl := range stxFile.Decls {
-				fnDecl, ok := decl.(*ast.FuncDecl)
-				if !ok ||
-					fnDecl.Type.Results == nil ||
-					len(fnDecl.Type.Results.List) == 0 {
-					continue
+			for _, d := range stxFile.Decls {
+				funcDecl, ok := d.(*ast.FuncDecl)
+				if !ok {
+					// It's a bug!
+					return fmt.Errorf("%s: expected a function declaration, found: %T %+v", filename, d, d)
 				}
 
-				rets := fnDecl.Type.Results.List
-				for _, ret := range rets {
-					// All types should implement the stringer interface
-					tp, ok := ret.Type.(fmt.Stringer)
+				if funcDecl.Body == nil {
+					// Shouldn't happen
+					return fmt.Errorf("%s: function declaration has no body: %s", filename, funcDecl.Name)
+				}
+
+				for _, stmt := range funcDecl.Body.List {
+					// Find only the return statements
+					returnStmt, ok := stmt.(*ast.ReturnStmt)
 					if !ok {
-						// Errors will always implement the Stringer interface
-						debugPrintf(filename, "return type is not a stringer: %s", ret.Type)
 						continue
 					}
 
-					if tp.String() == "error" {
-						// Found a function that returns an error,
-						// keep it in the declarations list
-						stxFile.Decls[j] = decl
-						j++
+					if len(returnStmt.Results) == 0 {
+						return fmt.Errorf("%s: return statement has no results: %s", filename, funcDecl.Name)
+					}
+
+					for _, ret := range returnStmt.Results {
+						debugPrintf("--- ret %+v", ret)
 					}
 				}
 			}
-			stxFile.Decls = stxFile.Decls[:j]
-			debugPrintf(filename, "num decls: %d", len(stxFile.Decls))
 
-			for _, d := range stxFile.Decls {
-				debugPrintf(filename, "%+v", d)
-			}
-
-			shouldKeep := len(stxFile.Decls) > 0
-			if !shouldKeep {
-				fmt.Println(filename, "no errors ")
-				pkg.Fset.RemoveFile(tokenFile)
-			}
 		}
 	}
 
 	return nil
 }
 
+func (g *Generator) filterPackageDecls(pkg *packages.Package) {
+	for _, stxFile := range pkg.Syntax {
+		filename := getFilename(pkg, stxFile)
+		debugPrintf, debugPrintln := debugPrint(filename)
+
+		// Filter functions that return an error
+		j := 0
+		for _, decl := range stxFile.Decls {
+			fnDecl, ok := decl.(*ast.FuncDecl)
+			if !ok ||
+				fnDecl.Type.Results == nil ||
+				len(fnDecl.Type.Results.List) == 0 {
+				continue
+			}
+
+			rets := fnDecl.Type.Results.List
+			for _, ret := range rets {
+				// All types should implement the stringer interface
+				tp, ok := ret.Type.(fmt.Stringer)
+				if !ok {
+					// Errors will always implement the Stringer interface
+					debugPrintf("return type is not a stringer: %s", ret.Type)
+					continue
+				}
+
+				if tp.String() == "error" {
+					// Found a function that returns an error,
+					// keep it in the declarations list
+					stxFile.Decls[j] = decl
+					j++
+				}
+			}
+		}
+		stxFile.Decls = stxFile.Decls[:j]
+		debugPrintf("num decls: %d", len(stxFile.Decls))
+
+		shouldKeep := len(stxFile.Decls) > 0
+		if !shouldKeep {
+			debugPrintln("no errors ")
+			tokenFile := pkg.Fset.File(stxFile.FileStart)
+			pkg.Fset.RemoveFile(tokenFile)
+		}
+	}
+}
+
 func (g *Generator) Generate(output string) error {
 	return nil
 }
 
-func debugPrintf(filename string, s string, a ...any) {
-	fmt.Printf(fmt.Sprintf("%s: %s\n", filename, s), a...)
+func getFilename(pkg *packages.Package, stxFile *ast.File) string {
+	tokenFile := pkg.Fset.File(stxFile.FileStart)
+	filename := tokenFile.Name()
+	return filename
 }
 
-func debugPrintln(filename string, s string, a ...any) {
-	args := []any{filename, s}
-	args = append(args, a...)
-	fmt.Println(args...)
+func debugPrint(filename string) (debugPrintf func(string, ...any), debugPrintln func(string, ...any)) {
+	printf := func(s string, a ...any) {
+		fmt.Printf(fmt.Sprintf("%s: %s\n", filename, s), a...)
+	}
+	println := func(s string, a ...any) {
+		args := []any{filename, s}
+		args = append(args, a...)
+		fmt.Println(args...)
+	}
+	return printf, println
 }
