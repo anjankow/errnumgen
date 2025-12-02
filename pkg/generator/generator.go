e2cf2d7440ad61788779761f002cd807dbb8c9e9
anjankow <anjankow@protonmail.com>
2025-12-02 11:30:18 +0100

    implementing generator

--- pkg/generator/generator.go
+++ pkg/generator/generator.go
@@ -3,6 +3,8 @@ package generator
 import (
 	"bytes"
 	"fmt"
+	"go/ast"
+	"go/parser"
 	"go/token"
 
 	"golang.org/x/tools/go/packages"
@@ -21,10 +23,26 @@ func New(dir string) (Generator, error) {
 		Mode:  packages.NeedSyntax | packages.NeedFiles | packages.NeedName,
 		Dir:   dir,
 		Tests: false,
+		ParseFile: func(fset *token.FileSet, filename string, data []byte) (*ast.File, error) {
+			// Check if there are any return statements in the file
+			if !bytes.Contains(data, []byte("return")) {
+				return nil, nil
+			}
+
+			const mode = parser.AllErrors | parser.SkipObjectResolution
+			return parser.ParseFile(fset, filename, data, mode)
+		},
 	}
-	pkgs, err := packages.Load(cfg)
+
+	// Load all nested packages within the directory
+	const patterns = "./..."
+	pkgs, err := packages.Load(cfg, patterns)
 	if err != nil {
 		return Generator{}, err
+	}
+
+	if cnt := packages.PrintErrors(pkgs); cnt > 0 {
+		return Generator{}, fmt.Errorf("failed to load %d packages", cnt)
 	}
 
 	if len(pkgs) == 0 {
@@ -37,17 +55,29 @@ func New(dir string) (Generator, error) {
 }
 
 func (g *Generator) ParseErrs() error {
-	for _, p := range g.pkgs {
-		fmt.Println(p.Dir)
-		p.Fset.Iterate(func(f *token.File) bool {
-			fmt.Println(f.Name())
-			fmt.Println("line count: ", f.LineCount())
-			start := f.LineStart(4)
-			stop := f.LineStart(5)
-			fmt.Println("start: ", start, "stop: ", stop)
-			return true
-		})
+	for _, pkg := range g.pkgs {
+		fmt.Println("PACKAGE NAME: ", pkg.Name)
+
+		for _, stxFile := range pkg.Syntax {
+			tokenFile := pkg.Fset.File(stxFile.FileStart)
+			filename := tokenFile.Name()
+
+			if stxFile.Decls == nil {
+				fmt.Println(filename, "  --> No decls")
+			}
+
+			shouldKeep := ast.FilterFile(stxFile, func(decl string) bool {
+				fmt.Println(filename, " -- decl: ", decl)
+				return true
+			})
+
+			if !shouldKeep {
+				fmt.Println(filename, " REMOVE ")
+				pkg.Fset.RemoveFile(tokenFile)
+			}
+		}
 	}
+
 	return nil
 }
 
